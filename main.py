5c9d17c11c59f6a83c4a2bdfdbc29c7bdbc4d420
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2025-03-24 13:38:07 +0200

    import all the function from data_entry.py to main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import W
 import pandas as pd
 import csv 
 from datetime import datetime
+from data_entry import get_amount, get_category, get_description, get_date
 
 class CSV:
     CSV_FILE = "finance_data.csv"
