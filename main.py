5bc93bc5968a183e9bcd32009915bf32e296b727
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2025-03-24 10:52:31 +0200

    import pandas,csv and datetime
    - create CSV class that contains financial data
    - initialize csv and use try and except to showcase those processes

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import csv 
+from datetime import datetime
+
+class CSV:
+    CSV_FILE = "finance_data.cv"
+
+    @classmethod
+    def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=["date", "amount", "category" , "description"])
+            df.to_csv(cls.CSV_FILE, index=False)
