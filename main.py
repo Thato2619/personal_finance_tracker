af4f2e03711df79e473dbfe6bdc41c16b453e10e
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2025-03-25 09:39:36 +0200

    work on logic of working with dates

--- main.py
+++ main.py
@@ -35,6 +35,19 @@ class CSV:
 def get_transactions(cls, start_date, end_date):
     df = pd.read.csv(cls.CSV_FILE)
     df["date"] = pd.to_datetime(df["date"], format=CSV.FORMAT)
+    start_date = datetime.strptime(start_date, CSV.FORMAT)
+    end_date = datetime.strptime(end_date, CSV.FORMAT)
+
+    mask = (df["date"] >= start_date & (df["date"]) <= end_date)
+    filtered_df = df.loc[mask]
+
+    if filtered_df.empty:
+        print("No transactions found in the given date range")
+    else:
+        print(f"Transactions from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}")
+        print(
+            filtered_df.to_string(index=False, formatters={"date": lambda x:x.strftime(CSV.FORMAT)})
+            )
 def add():
     CSV.initialize_csv()
     date = get_date("Enter the date of the transacrion (dd-mm-yyyy) or enter for today;s date: ", allow_default=True)
